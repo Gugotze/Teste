3e1d53a75ef6e9ff9d9de8c7c235688109b00c7e
Gustavo Fonseca Pitombeira <gustavofonsecap@hotmail.com>
2025-05-01 08:25:47 -0300

    Novo teste

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,29 @@
 AOS.init();
-
-const dataDoEvento = new Date('Dec 12, 2025 19:00:00');
+const dataDoEvento = new Date("apr 17, 2026 19:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
-const contaAsHoras = setInterval(function() {
+const contaAsHoras = setInterval(function(){
     const agora = new Date();
     const timeStampAtual = agora.getTime();
 
     const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
 
-    const diaEmMs = 1000*60*60*24;
-    const horaEmMs = 1000*60*60;
-    const minutoEmMs = 1000*60;
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
 
-    const diasAteOEvento = Math.floor(distanciaAteOEvento/diaEmMs);
-    const horasAteOEvento = Math.floor((distanciaAteOEvento%diaEmMs)/horaEmMs);
-    const minutosAteOEvento = Math.floor((distanciaAteOEvento%horaEmMs)/minutoEmMs);
-    const segundosAteOEvento = Math.floor((distanciaAteOEvento%minutoEmMs)/1000);
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutosEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutosEmMs) / 1000);
 
-    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+    document.getElementById('dia').innerText = `${diasAteOEvento}`
+    document.getElementById('hora').innerText = `${horasAteOEvento}`
+    document.getElementById('minuto').innerText = `${minutosAteOEvento}`
+    document.getElementById('segundo').innerText = `${segundosAteOEvento}`
 
-    if (diasAteOEvento < 0) {
+    if(distanciaAteOEvento < 0){
         clearInterval(contaAsHoras);
-        document.getElementById('contador').innerHTML = 'Evento expirado';
+        document.getElementById('contador').innerHTML = `Evento expirado`
     }
-}, 1000);
+},1000);
